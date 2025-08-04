04a979aec3b29b9233d0ee6aaa27bb03e9635a7a
Niko <nikojlarin22@gmail.com>
2025-08-03 20:23:45 -0400

    Create account.js

---
+++ api/account.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const { ALPACA_API_KEY_ID, ALPACA_SECRET_KEY } = process.env;
+
+  const response = await fetch('https://paper-api.alpaca.markets/v2/account', {
+    headers: {
+      'APCA-API-KEY-ID': ALPACA_API_KEY_ID,
+      'APCA-API-SECRET-KEY': ALPACA_SECRET_KEY
+    }
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
