7ef997b70088de1405fb4a9b1c35fc070a79318a
Niko <nikojlarin22@gmail.com>
2025-08-04 12:18:15 -0400

    Update portfolio_beta.js

--- api/portfolio_beta.js
+++ api/portfolio_beta.js
@@ -20,7 +20,9 @@ export default async function handler(req, res) {
     const betaData = await betaRes.json();
 
     const beta = betaData.metric && betaData.metric.beta ? parseFloat(betaData.metric.beta) : 1;
-    betaSum += beta * market_value;
+    const direction = market_value >= 0 ? 1 : -1;
+    betaSum += beta * Math.abs(market_value) * direction;
+
     totalValue += market_value;
   }
 
